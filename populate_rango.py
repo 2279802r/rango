bf019c56626c7f16269160e207f5349053cf01c6
Jiaqing Ran <2279802r@student.gla.ac.uk>
2017-01-26 22:22:30 +0000

    Chapter 6 completed

--- populate_rango.py
+++ populate_rango.py
@@ -16,32 +16,32 @@ def populate():
     python_pages = [
         {"title": "Official Python Tutorial",
          "url":"http://docs.python.org/2/tutorial/",
-         "views": 0},
+         "views": 32},
         {"title":"How to Think like a Computer Scientist",
          "url":"http://www.greenteapress.com/thinkpython/",
-         "views": 0},
+         "views": 16},
         {"title":"Learn Python in 10 Minutes",
          "url":"http://www.korokithakis.net/tutorials/python/",
-         "views": 0}]
+         "views": 8}]
 
     django_pages = [
         {"title":"Official Django Tutorial",
          "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
-         "views":0},
+         "views":32},
         {"title":"Django Rocks",
          "url":"http://www.djangorocks.com/",
-         "views":0},
+         "views":16},
         {"title":"How to Tango with Django",
          "url":"http://www.tangowithdjango.com/",
-         "views":0} ]
+         "views":8} ]
 
     other_pages = [
         {"title":"Bottle",
          "url":"http://bottlepy.org/docs/dev/",
-         "views":0},
+         "views":32},
         {"title":"Flask",
          "url":"http://flask.pocoo.org",
-         "views":0} ]
+         "views":16} ]
 
     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
             "Django": {"pages": django_pages,"views":64,"likes":32},
